ba4fc653d03de7bffa0dbab77d8eb4be190ed8b5
Muhammad Aswan <muhaswan190897@gmail.com>
2022-08-20 20:05:03 +0800

    move page using component Link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
+    <>
+    <header>
+      <ul>
+        <li><Link href="/"><a>Home</a></Link></li>
+        <li><Link href="/blog"><a>Blog</a></Link></li>
+        <li><Link href="/users"><a>Users</a></Link></li>
+      </ul>
+    </header>
     <h1>Welcome Aswan</h1>
+    </>
   )
 }
